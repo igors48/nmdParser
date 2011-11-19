94a58a32a0c76a83511dc9df72ce84ea73d56236
Igor <nmds48@gmail.com>
2011-11-19 20:41:04 +0200

    Work on PasswordEncoder

--- src/greader/profile/PasswordEncoder.java
+++ src/greader/profile/PasswordEncoder.java
@@ -1,15 +1,19 @@
 package greader.profile;
 
 import util.Assert;
+import util.Base64;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.io.ByteArrayOutputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Author: Igor Usenko ( igors48@gmail.com )
  * Date: 18.11.2011
  */
 public class PasswordEncoder extends XmlAdapter<String,String> {
-    
+    private static final String UTF_8_CHARSET = "UTF-8";
+
     @Override
     public String unmarshal(final String _value) throws Exception {
         Assert.notNull(_value, "Value is null");
@@ -25,7 +29,14 @@ public class PasswordEncoder extends XmlAdapter<String,String> {
     }
     
     private String reverse(final String _value) {
-        return new StringBuilder(_value).reverse().toString();
+        return _value;
+        //return new StringBuilder(_value).reverse().toString();
     }
+
+//    private String gzip(final String _value) {
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        byteArrayOutputStream.write(_value.getBytes(UTF_8_CHARSET));
+//        GZIPOutputStream gzipOutputStream = new GZIPOutputStream()
+//    }
 
 }
